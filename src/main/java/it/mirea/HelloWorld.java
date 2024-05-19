d635237826cda9e29f3e1aee3b2d03685a7ca478
mikhi <mikhienkov@inbox.ru>
2024-05-19 15:23:27 +0300

    Initial commit

---
+++ src/main/java/it/mirea/HelloWorld.java
@@ -0,0 +1,12 @@
+package it.mirea;
+
+import io.javalin.Javalin;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        var app = Javalin.create(/*config*/)
+                .get("/", ctx -> ctx.result("Hello World"))
+                .start(7070);
+    }
+}
+
