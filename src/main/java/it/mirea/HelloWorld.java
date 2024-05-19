e892962ae598cb4036365e25c8cbe37b1b4adf1a
mikhi <mikhienkov@inbox.ru>
2024-05-19 17:12:44 +0300

    change build.yml

--- src/main/java/it/mirea/HelloWorld.java
+++ src/main/java/it/mirea/HelloWorld.java
@@ -5,7 +5,7 @@ import io.javalin.Javalin;
 public class HelloWorld {
     public static void main(String[] args) {
         var app = Javalin.create(/*config*/)
-                .get("/", ctx -> ctx.result("Hello World!!!!!!"))
+                .get("/hello", ctx -> ctx.result("{ \"answer\" : \"Hello World!\" }\n"))
                 .start(7070);
     }
 }
